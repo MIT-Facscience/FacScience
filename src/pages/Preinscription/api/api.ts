adf66c76d2b78452d478008ff5dc7776180a3a1c
damyeden <teh01243@gmail.com>
2025-10-08 15:28:13 +0300

    change fetch

--- src/pages/Preinscription/api/api.ts
+++ src/pages/Preinscription/api/api.ts
@@ -1,13 +1,12 @@
-import type { ForeignApplicationData } from './../types/index';
-import type { ApplicationData } from '../types';
-import type { ResData } from '../types/models';
-import { BACKEND_WEBSERVICES_URL } from '@/lib/api';
-import { BACKEND_PREINSCRIPTION_URL } from '@/lib/api';
+import { BACKEND_PREINSCRIPTION_URL, BACKEND_WEBSERVICES_URL } from "@/lib/api";
+import type { ApplicationData } from "../types";
+import type { ResData } from "../types/models";
+import type { ForeignApplicationData } from "./../types/index";
 
 // import { Preinscription, ResData } from '../types/models';
 
 // Simulate API delay
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const fetchCandidateInfo = async (
   num_bacc: string,
@@ -15,28 +14,31 @@ export const fetchCandidateInfo = async (
 ) => {
   await delay(1000); // Simulate network delay
 
-  const res = await fetch(`${BACKEND_WEBSERVICES_URL}/preinscription`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({num_bacc, annee_bacc})
-  })
+  const res = await fetch(
+    `${BACKEND_WEBSERVICES_URL}/api/preinscription/getDataByNumBacc`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ num_bacc, annee_bacc }),
+    }
+  );
   if (!res.ok) {
-    throw new Error("request Error")
+    throw new Error("request Error");
   }
   const candidate: ResData = await res.json();
-  
+
   // const candidate = mockCandidates.find(
-  //   c => c.num_bacc === num_bacc && 
+  //   c => c.num_bacc === num_bacc &&
   //        c.graduationYear === annee_bacc
   // );
-  
+
   if (!candidate) {
-    throw new Error('Candidat introuvable avec ces informations');
+    throw new Error("Candidat introuvable avec ces informations");
   }
-  
-  return candidate
+
+  return candidate;
 };
 
 export const submitApplication = async (applicationData: ApplicationData) => {
@@ -44,74 +46,78 @@ export const submitApplication = async (applicationData: ApplicationData) => {
   // await new Promise(resolve => setTimeout(resolve, 2000));
   try {
     const app: ApplicationData = applicationData;
-    const response = await fetch(`${BACKEND_PREINSCRIPTION_URL}/api/Register/RegisterTry`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(app),
-    });
+    const response = await fetch(
+      `${BACKEND_PREINSCRIPTION_URL}/api/Register/RegisterTry`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(app),
+      }
+    );
 
     console.log(await response.json());
-    if (!response.ok) 
-      return {success: false};
-
+    if (!response.ok) return { success: false };
   } catch (error) {
     throw new Error("sending data error");
     console.error(error);
-    
+
     return {
       success: false,
       // applicationNumber: null,
     };
   }
-  
+
   // Simulation d'une réponse réussie
-  return { 
-    success: true, 
-    // applicationNumber: `FS${new Date().getFullYear()}${Math.floor(Math.random() * 10000).toString().padStart(4, '0')}` 
+  return {
+    success: true,
+    // applicationNumber: `FS${new Date().getFullYear()}${Math.floor(Math.random() * 10000).toString().padStart(4, '0')}`
   };
 };
 
-export const submitForeignPreinscription = async (applicationData: ForeignApplicationData) => {
+export const submitForeignPreinscription = async (
+  applicationData: ForeignApplicationData
+) => {
   try {
-    const app: ForeignApplicationData= applicationData;
-    const response = await fetch(`${BACKEND_PREINSCRIPTION_URL}/api/Register/RegisterTry`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(app),
-    });
+    const app: ForeignApplicationData = applicationData;
+    const response = await fetch(
+      `${BACKEND_PREINSCRIPTION_URL}/api/Register/RegisterTry`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(app),
+      }
+    );
 
-    if (!response.ok) 
-      return {success: false};
+    if (!response.ok) return { success: false };
 
     console.log(await response.json());
-
   } catch (error) {
     throw new Error("sending data error");
     console.error(error);
-    
+
     return {
       success: false,
       // applicationNumber: null,
     };
   }
-  
+
   // Simulation d'une réponse réussie
-  return { 
-    success: true, 
-    // applicationNumber: `FS${new Date().getFullYear()}${Math.floor(Math.random() * 10000).toString().padStart(4, '0')}` 
+  return {
+    success: true,
+    // applicationNumber: `FS${new Date().getFullYear()}${Math.floor(Math.random() * 10000).toString().padStart(4, '0')}`
   };
-}
+};
 
 export const isValidEmail = (email: string): boolean => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email.trim());
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email.trim());
 };
 
 export const isValidTel = (tel: string): boolean => {
-  const telRegex = /^(?:\+261\d{9}|(?:020|032|033|034|037)\d{7})$/
-  return telRegex.test(tel.trim().replace(/\s+/g, ""))
-}
+  const telRegex = /^(?:\+261\d{9}|(?:020|032|033|034|037)\d{7})$/;
+  return telRegex.test(tel.trim().replace(/\s+/g, ""));
+};
